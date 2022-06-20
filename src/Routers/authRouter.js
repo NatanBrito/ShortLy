382f8d0a67937293f57f8599f52897ce9601238f
Guga <contato.gustavobarbosa@outlook.com>
2022-06-20 15:45:39 -0300

    Preparando revisão de código

--- src/Routers/authRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signUP, signIn } from "../Controllers/authController.js";
-import {ValidateSignUp,ValidateSignIn} from "../Middlewares/authValidateMiddleware.js";
-const authRouter = Router();
-
-authRouter.post('/signup', ValidateSignUp, signUP);
-authRouter.post('/signin',ValidateSignIn, signIn);
-export default authRouter;
